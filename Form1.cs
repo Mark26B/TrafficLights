aaeaf80198846d50dfcbbdd7c69ebbb6aa6c7311
Marks Rozītis <rozitismarks@gmail.com>
2020-06-03 18:07:47 +0300

    version 0.1 RED YELLOW GREEN

--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,42 @@ using System.Windows.Forms;
 
 namespace TrafficLights
 {
-    public partial class Form1 : Form
+    public partial class TrafficLights : Form
     {
-        public Form1()
+        private Timer timerSwitch;
+
+        public TrafficLights()
         {
             InitializeComponent();
+            InitializeTrafficLights();
+            InitializeTimerSwitch();
+        }
+
+        private void InitializeTimerSwitch()
+        {
+            timerSwitch = new Timer();
+            timerSwitch.Interval = 1000;
+            timerSwitch.Tick += new EventHandler(TimerSwitch_Tick);
+            timerSwitch.Start();
+        }
+
+        private void TimerSwitch_Tick(object sender, EventArgs e)
+        {
+            if(redLight.BackColor == Color.Gray)
+            {
+                redLight.BackColor = Color.Red;
+            }
+            else
+            {
+                redLight.BackColor = Color.Gray;
+            }
+        }
+
+        private void InitializeTrafficLights()
+        {
+            redLight.BackColor = Color.Gray;
+            yellowLight.BackColor = Color.Gray;
+            greenLight.BackColor = Color.Gray;
         }
     }
 }
